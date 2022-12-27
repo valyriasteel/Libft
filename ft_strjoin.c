12a6ae8b5d460eee8497f3ddbdd8ccbaaab15e76
valyriasteel <beratbosnak@hotmail.com>
2022-12-27 17:03:19 +0300

    first project

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -1,39 +1,45 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: bbosnak <bbosnak@student.42kocaeli.com.tr  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/12/27 15:54:10 by bbosnak           #+#    #+#             */
-/*   Updated: 2022/12/27 15:54:14 by bbosnak          ###   ########.tr       */
+/*   Created: 2022/12/27 16:59:44 by bbosnak           #+#    #+#             */
+/*   Updated: 2022/12/27 16:59:45 by bbosnak          ###   ########.tr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_substr(char const *s, unsigned int start, size_t len)
+char	*ft_strjoin(char const *s1, char const *s2)
 {
 	char	*new_str;
-	size_t	i;
-	size_t	j;
+	int		i;
+	int		j;
 
-	i = start;
-	j = 0;
-	new_str = (char *)malloc(len + 1);
-	if (!s || !new_str)
+	if (!s1 || !s2)
 		return (0);
-	while (i < ft_strlen(s) && j < len)
+	new_str = (char *)malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	if (!new_str)
+		return (NULL);
+	i = 0;
+	while (s1[i] != '\0')
 	{
-		new_str[j++] = s[i++];
+		new_str[i] = s1[i];
+		i++;
 	}
-	new_str[j] = '\0';
+	j = 0;
+	while (s2[j] != '\0')
+		new_str[i++] = s2[j++];
+	new_str[i] = '\0';
 	return (new_str);
 }
 
 /* int	main()
 {
-	char	var[] = "berat";
+	char	var[] = "hello";
+	char	var2[] = " man";
 
-	printf("%s", ft_substr(var, 0, 5));
+	printf("%s", ft_strjoin(var, var2));
 } */
